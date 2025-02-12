f076ee7d3b009e3ec947532b8edab68fe242f680
Rajyalakshmi Koiguri <44266931+raji2506@users.noreply.github.com>
2025-02-12 22:12:30 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,6 +7,6 @@ variable "vpc_cidr" {
   description = "default CIDR range of the VPC"
 }
 variable "kubernetes_version" {
-  default     = 1.27
+  default     = 1.28
   description = "kubernetes version"
 }

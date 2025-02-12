7937a98cbcfce6c373806198930dc15df26d6860
Rajyalakshmi Koiguri <44266931+raji2506@users.noreply.github.com>
2025-02-12 13:55:20 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  default = "us-east-1"
+  description = "aws region"
+}
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "default CIDR range of the VPC"
+}
+variable "kubernetes_version" {
+  default     = 1.27
+  description = "kubernetes version"
+}

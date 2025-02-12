cb9ca2cd9d192f1ecec4a576dd6eaeef259f4fcf
Rajyalakshmi Koiguri <44266931+raji2506@users.noreply.github.com>
2025-02-12 22:11:52 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "raji-eks-${random_string.suffix.result}"
+  cluster_name = "eks-cluster-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -21,6 +21,9 @@ module "vpc" {
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
